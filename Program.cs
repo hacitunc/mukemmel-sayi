966aaf5a55e92b6ffd804ef7efd67c9339beeb0c
hacitunc <hacitunc8323@gmail.com>
2018-02-25 19:01:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mukemmel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+
+           for(int i = 1; i<sayi; i++)
+            {
+                if(sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+
+                else
+                {
+                    i++;
+                }
+            }
+
+           if(toplam == sayi)
+            {
+                Console.WriteLine(sayi + " sayısı Mükemmel Sayıdır");
+            }
+
+            else
+            {
+                Console.WriteLine(sayi + " sayısı Mükemmel Sayı Değildir");
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
